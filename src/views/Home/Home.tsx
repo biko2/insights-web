d445198c5932a7d7bd4b820090f93b90726ae634
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-10-26 16:34:10 +0200

    feat: estiliza frases con css modules

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
+
+import styles from './home.module.scss';
 
 export const Home: React.FC = () => {
-  return <h1>Hello, World! 🌍🙋🏽‍♀️</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>Hello, World! 🌍🙋🏽‍♀️</h1>
+      <p className={styles.text}>Aquí irá la web de los Biko Insights</p>
+    </>
+  );
 };
