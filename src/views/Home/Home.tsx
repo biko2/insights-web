bb7fec1e33c4e4628901fa88ab1a31c04688f388
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-10-26 12:21:58 +0200

    chore: añade prettier

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export const Home: React.FC = () => {
-    return <h1>Hello, World! 🌍🙋🏽‍♀️</h1>
-}
+  return <h1>Hello, World! 🌍🙋🏽‍♀️</h1>;
+};
