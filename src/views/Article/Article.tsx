ce4337f15bcf3edd8849ac22193b503ae2a33305
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-11-06 19:13:16 +0100

    feat: añade header y footer

--- src/views/Article/Article.tsx
+++ src/views/Article/Article.tsx
@@ -5,8 +5,60 @@ import styles from "./Article.module.scss";
 
 export const Article: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <main className={`${containerStyles.container} ${styles.wrapper}`}>
-      {children}
-    </main>
+    <>
+      <header className={styles.header}>
+        <img
+          className={styles.heroLogo}
+          src="/logo-bikoinsights.svg"
+          alt="Logo de Jakala BikoInsights"
+        />
+
+        <nav>
+          <ul className={styles.headerItems}>
+            <li>
+              <a href="#sumario" className={styles.headerItem}>
+                Sumario{" "}
+                <svg
+                  style={{ width: 24, height: 24 }}
+                  role="img"
+                  aria-hidden="true"
+                >
+                  <use xlinkHref={"/sprite.svg#arrow-down"} />
+                </svg>
+              </a>
+            </li>
+
+            <li>
+              <a href="#ediciones-anteriores" className={styles.headerItem}>
+                Ediciones anteriores{" "}
+                <svg
+                  style={{ width: 24, height: 24 }}
+                  role="img"
+                  aria-hidden="true"
+                >
+                  <use xlinkHref={"/sprite.svg#arrow-down"} />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className={`${containerStyles.container} ${styles.wrapper}`}>
+        {children}
+      </main>
+
+      <footer className={styles.footer}>
+        <div className={containerStyles.container}>
+          <div className={styles.footerWrapper}>
+            <img src="icons/jakala.svg" alt="Logo de Jakala" />
+            <p className={styles.footerText}>
+              BikoInsights es un documento sobre tendencias digitales elaborado
+              por Jakala | Biko con carácter divulgativo.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
