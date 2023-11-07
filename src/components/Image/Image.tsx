d01b4471760e258846db0b1d499ba30bea678710
Aitor Urrutia Aranburu <aitor.urrutia@biko2.com>
2023-11-07 18:09:30 +0100

    feat: optimiza imágenes de markdown

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -2,17 +2,6 @@ import React from 'react';
 
 import styles from './Image.module.scss';
 
-interface Props {
-  panoramicSrc: string;
-  squareSrc: string;
-  alt: string;
-}
-
-export const Image: React.FC<Props> = ({ panoramicSrc, squareSrc, alt }) => {
-  return (
-    <picture>
-      <source media="(min-width:1024px)" srcSet={panoramicSrc} />
-      <img className={styles.wrapper} src={squareSrc} alt={alt}></img>
-    </picture>
-  );
+export const Image: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <div className={styles.wrapper}>{children}</div>;
 };
