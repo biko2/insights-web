eb712b7d98fa5a917ecdf86cfc483a7cef8fc89c
María B <mariab@M2M003.local>
2023-11-21 13:31:53 +0100

    feat: prueba rename fichero para la build

--- src/components/NumberedSectionTitle/NumberedSectionTitle.tsx
+++ src/components/NumberedSectionTitle/NumberedSectionTitle.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './numberedSectionTitle.module.scss';
+import styles from './NumberedSectionTitle.module.scss';
 
 interface Props {
   number: number;
