fdd2914152e47078174bda2e8061dec3497eb46c
albabarrera <alba.barrera@biko2.com>
2023-12-13 12:22:52 +0100

    feat: añade espaciado especial entre párrafos

---
+++ src/components/ParagraphSpecialSpacing/ParagraphSpecialSpacing.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./ParagraphSpecialSpacing.module.scss";
+
+export const ParagraphSpecialSpacing: React.FC = () => {
+  return <div className={styles.spacing}></div>;
+};
---
+++ src/components/ParagraphSpecialSpacing/index.ts
@@ -0,0 +1,1 @@
+export { ParagraphSpecialSpacing } from "./ParagraphSpecialSpacing";
