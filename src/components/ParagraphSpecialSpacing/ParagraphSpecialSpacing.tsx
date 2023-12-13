da68b8f02a4e7d3889d68f1d7e65a36a66c7e4d7
albabarrera <alba.barrera@biko2.com>
2023-12-13 16:41:20 +0100

    refactor: ajusta espaciado entre párrafos y elimina componente innecesario

--- src/components/ParagraphSpecialSpacing/ParagraphSpecialSpacing.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import styles from "./ParagraphSpecialSpacing.module.scss";
-
-export const ParagraphSpecialSpacing: React.FC = () => {
-  return <div className={styles.spacing}></div>;
-};
--- src/components/ParagraphSpecialSpacing/index.ts
+++
@@ -1,1 +0,0 @@
-export { ParagraphSpecialSpacing } from "./ParagraphSpecialSpacing";
